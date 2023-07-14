13088f62ed9ccfe6a7f757dcd40455c2655e9323
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-14 17:52:32 -0400

    region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.aws_credentials = {
   access_key_id: ENV['aws_access_key_id'],
   secret_access_key: ENV['aws_secret_access_key'],
-  region: ENV['AWS_REGION']
+  region: ENV['aws_region']
 }
 
   
