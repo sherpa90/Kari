ced432805fe4f97d13a6dfd0c41e9ebd8958e26a
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-14 04:18:20 -0400

    resuelve migracion

--- db/schema.rb
+++ db/schema.rb
@@ -63,14 +63,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_052903) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_admin"
-    t.string "name"
-    t.string "profile_picture"
+    t.boolean "is_admin", default: false
     t.datetime "confirmed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
