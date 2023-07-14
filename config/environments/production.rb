42a658931c671f878e64b75354ae37a7a1da37b9
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-14 17:38:51 -0400

    us-east

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.aws_credentials = {
   access_key_id: '<%= ENV['aws_access_key_id'] %>',
   secret_access_key: '<%= ENV['aws_secret_access_key'] %>',
-  region: '<%= ENV['aws_region'] %>'
+  region: 'us-east-1'
 }
   
   # Mount Action Cable outside main process or domain.
