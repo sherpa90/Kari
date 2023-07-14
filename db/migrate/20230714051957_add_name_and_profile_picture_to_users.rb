66306865be095ad266b9e642c5bca2c890960382
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-14 04:24:21 -0400

    veamos

--- db/migrate/20230714051957_add_name_and_profile_picture_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndProfilePictureToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :profile_picture, :string
-  end
-end
