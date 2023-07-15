34e58575f9fb51e678d51040d029501c2b18e6b9
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-14 20:06:48 -0400

    true

--- db/migrate/20230714050100_add_is_admin_to_users.rb
+++ db/migrate/20230714050100_add_is_admin_to_users.rb
@@ -1,5 +1,5 @@
 class AddIsAdminToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :is_admin, :boolean, default: false
+    add_column :users, :is_admin, :boolean, default: true
   end
 end
