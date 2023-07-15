be22f82a708a6f65b0b6153e9899367eb3b6543e
sherpa90 <marco.rosasvillarroel@gmail.com>
2023-07-14 20:15:22 -0400

    2

--- db/schema.rb
+++ db/schema.rb
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_14_052903) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_admin", default: false
+    t.string "profile_picture"
     t.datetime "confirmed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
